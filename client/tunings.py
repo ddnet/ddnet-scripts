5aae00ae8e0a6b90e344c0ad9daccbc4ae6e7500
def <dennis@felsin9.de>
2015-05-16 16:02:29 +0200

    Update client tunings

--- client/tunings.py
+++ client/tunings.py
@@ -5,10 +5,16 @@ import sys
 print '<table class="settingscommands">'
 print '  <tr><th>Tuning</th><th>Description</th><th>Default</th></tr>'
 
+def foo(x):
+  if x.endswith("f / TicksPerSecond"):
+    x = x.rstrip("f / TicksPerSecond")
+    return float(x) / 50.0
+  return x.strip("f")
+
 for line in sys.stdin:
   x = line.split(",")
 
-  result = (x[1].strip(), x[3].strip(" )\n").split('"')[-2], x[2].strip("f"))
+  result = (x[1].strip(), x[3].strip(" )\n").split('"')[-2], foo(x[2]))
 
   print '  <tr><td>%s</td><td>%s</td><td>%s</td></tr>' % result
 
