aea9ac99068b0ceeff650270bd674911d9d83c1f
def <dennis@felsin9.de>
2016-04-27 21:35:30 +0200

    Meh, password

--- servers/scripts/check-mysql.sh
+++ servers/scripts/check-mysql.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-text=$(echo "show slave status\G" | mysql -u repl -p'vmx112939f0v!MMM')
+text=$(echo "show slave status\G" | mysql -u repl -p'SECRETSLAVE')
 echo "$text" | grep -q "Waiting for master to send event" && exit 0
 echo "$text" | grep -q "Checking master version" && exit 0
 echo "$text" | grep -q "Reconnecting after a failed master event read" && exit 0
 sleep 300
-text=$(echo "show slave status\G" | mysql -u repl -p'vmx112939f0v!MMM')
+text=$(echo "show slave status\G" | mysql -u repl -p'SECRETSLAVE')
 echo "$text" | grep -q "Waiting for master to send event" && exit 0
 echo "$text" | grep -q "Checking master version" && exit 0
 echo "$text" | grep -q "Reconnecting after a failed master event read" && exit 0
