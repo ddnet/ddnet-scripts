748c9e147437a06f55f937ac8df4f32026c8b738
HMH <henry@freedesk.net>
2018-07-06 16:25:58 +0200

    Add arguments of commands to Settings & Commands docs.

--- client/commands.py
+++ client/commands.py
@@ -4,7 +4,7 @@ import sys
 import re
 
 print '<table class="settingscommands">'
-print '  <tr><th>Command</th><th>Description</th></tr>'
+print '  <tr><th>Command</th><th>Arguments</th><th>Description</th></tr>'
 
 for line in sys.stdin:
   if sys.argv[1] not in line:
@@ -13,8 +13,8 @@ for line in sys.stdin:
   x = re.findall(r'(?:[^\s,"]|"(?:\\.|[^"])*")+', line)
   y = line.split('"')
 
-  result = (y[1], y[-2])
+  result = (y[1], y[3], y[-2])
 
-  print '  <tr><td>%s</td><td>%s</td></tr>' % result
+  print '  <tr><td>%s</td><td>%s</td><td>%s</td></tr>' % result
 
 print '</table>'
