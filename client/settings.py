0255827030531dfd90a29d59481682edf9d66d78
Dennis Felsing <dennis.felsing@sap.com>
2018-10-31 08:28:02 +0100

    Support multiple config values inside of ifdefs
    As noticed by xse in https://github.com/ddnet/ddnet-web/pull/64

--- client/settings.py
+++ client/settings.py
@@ -6,8 +6,7 @@ import re
 print '<div style="overflow: auto;"><table class="settingscommands">'
 print '  <tr><th>Setting</th><th>Description</th><th>Default</th></tr>'
 
-lastname = None
-
+names = {}
 results = []
 
 for line in sys.stdin:
@@ -18,18 +17,19 @@ for line in sys.stdin:
   y = line.split('"')
   name = x[1].lstrip()
 
-  if lastname == name:
-    # Always use the last one we find, first ones are platform specific ifdefs
-    results.pop()
-
-  lastname = name
-
   if "STR" in line:
     result = (name, y[-2], x[3].lstrip())
   elif "INT" in line:
     result = (name, y[-2], x[-5].lstrip())
 
-  results.append('  <tr><td>%s</td><td>%s</td><td>%s</td></tr>' % result)
+  line = '  <tr><td>%s</td><td>%s</td><td>%s</td></tr>' % result
+
+  if name in names:
+    # Always use the last one we find, first ones are platform specific ifdefs
+    results[names[name]] = line
+  else:
+    names[name] = len(results)
+    results.append(line)
 
 for line in results:
   print line
