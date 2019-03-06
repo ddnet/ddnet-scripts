1e547d87bfcf8e14045d3ef9c7e035ec8c8e8ba9
Dennis Felsing <dennis.felsing@sap.com>
2019-03-06 07:50:43 +0100

    Simplify teehistorian index.sh script
    Requires https://github.com/heinrich5991/libtw2/commit/38b0f6d4414872c80ec908f52c2a3ba849e3110d

--- teehistorian/index.sh
+++ teehistorian/index.sh
@@ -1,2 +1,2 @@
-#!/usr/bin/env zsh
-xzcat "$1" > /tmp/${1:t:r} && ./teehistorian_index /tmp/${1:t:r} | tail -n-1 | cut -d',' -f2- >> ${1:h}/index.txt && rm /tmp/${1:t:r}
+#!/bin/sh
+xzcat "$1" | /media/teehistorian/teehistorian_index --ignore-ext - | tail -n-1 | cut -d',' -f2- >> $(dirname "$1")/index.new.txt
