3d15de5161c99f776220745bd776302fa6e51c91
Dennis Felsing <dennis.felsing@sap.com>
2019-03-06 07:53:43 +0100

    upd

--- teehistorian/index.sh
+++ teehistorian/index.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xzcat "$1" | /media/teehistorian/teehistorian_index --ignore-ext - | tail -n-1 | cut -d',' -f2- >> $(dirname "$1")/index.new.txt
+xzcat "$1" | /media/teehistorian/teehistorian_index --ignore-ext /dev/stdin | tail -n-1 | cut -d',' -f2- >> $(dirname "$1")/index.new.txt
