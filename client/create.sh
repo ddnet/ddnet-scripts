b72677a63162ebe033600e25ebe5299724cf7f37
Dennis Felsing <dennis.felsing@sap.com>
2019-05-21 10:40:05 +0200

    settingscommands script udpate

--- client/create.sh
+++ client/create.sh
@@ -2,7 +2,7 @@
 DIR=${0:h}
 echo '<div class="block">'
 echo '<h2 id="map-settings">Map Settings</h2>'
-cat **/{config_variables,variables}.h | $DIR/settings.py CFGFLAG_GAME
+cat src/**/{config_variables,variables}.h | $DIR/settings.py CFGFLAG_GAME
 cat src/**/{console,netban,server,gamecontext,engine}.cpp | grep "Register(" | grep CFGFLAG_GAME | grep -v "\/\/" | $DIR/commands.py CFGFLAG_GAME
 echo '</div>'
 
@@ -13,17 +13,17 @@ echo '</div>'
 
 echo '<div class="block">'
 echo '<h2 id="server-commands">Server Commands</h2>'
-cat src/**/{console,netban,server,gamecontext,engine}.cpp | grep "Register(" | grep CFGFLAG_SERVER | grep -v "\/\/" | $DIR/commands.py CFGFLAG_SERVER
+(cat src/**/{console,netban,server,gamecontext,engine}.cpp | grep "Register(" | grep CFGFLAG_SERVER | grep -v "\/\/"; cat src/**/ddracecommands.h) | $DIR/commands.py CFGFLAG_SERVER
 echo '</div>'
 
 echo '<div class="block">'
 echo '<h2 id="chat-commands">Chat Commands</h2>'
-cat **/ddracechat.h | $DIR/commands.py CFGFLAG_CHAT
+cat src/**/{ddracechat,ddracecommands}.h | $DIR/commands.py CFGFLAG_CHAT
 echo '</div>'
 
 echo '<div class="block">'
 echo '<h2 id="client-settings">Client Settings</h2>'
-cat **/variables.h **/config_variables.h | $DIR/settings.py CFGFLAG_CLIENT
+cat src/**/{variables,config_variables}.h | $DIR/settings.py CFGFLAG_CLIENT
 echo '</div>'
 
 echo '<div class="block">'
