c571df087d43f8be9216cc206d9e993e8a552b1c
def <dennis@felsin9.de>
2021-08-19 12:25:23 +0200

    Update ddnet-setup.sh for Debian 11

--- ddnet-setup.sh
+++ ddnet-setup.sh
@@ -2,7 +2,7 @@
 
 # From root @ other server:
 # cd /
-# tar --exclude='UBSAN*' --exclude='ASAN*' --exclude='ddnet-server*.sqlite' --exclude='ddnet-block.sqlite' --exclude='teehistorian' --exclude='*.sql' --exclude='*.log*' --exclude='*.fifo' --exclude='nohup.out' -czf ddnet-setup.tar.gz home/teeworlds/servers home/teeworlds/servers home/teeworlds/dnsbl home/teeworlds/run-all.sh home/teeworlds/.config home/teeworlds/.vim* home/teeworlds/.ssh* home/teeworlds/.z* home/teeworlds/.my.cnf home/teeworlds/.gitconfig etc/zsh etc/vim* etc/mysql etc/ssmtp etc/dnsmasq.d etc/dnsmasq.conf etc/resolv.dnsmasq.conf etc/resolv.conf etc/systemd/system/dnsbl-iphub.service etc/security/limits.conf root/weekly root/.config root/.vim* root/.ssh* root/.z* usr/local/bin/ni usr/local/bin/rni etc/init.d/teeworlds-servers etc/network/if-up.d/iptables etc/apt/apt.conf.d/99defaultrelease etc/apt/sources.list.d etc/timezone etc/apt/apt.conf.d/50unattended-upgrades var/spool/cron/crontabs
+# tar --exclude='UBSAN*' --exclude='ASAN*' --exclude='ddnet-server*.sqlite' --exclude='ddnet-block.sqlite' --exclude='teehistorian' --exclude='*.sql' --exclude='*.log*' --exclude='*.fifo' --exclude='nohup.out' -czf ddnet-setup.tar.gz home/teeworlds/servers home/teeworlds/servers home/teeworlds/dnsbl home/teeworlds/run-all.sh home/teeworlds/.config home/teeworlds/.vim* home/teeworlds/.ssh* home/teeworlds/.z* home/teeworlds/.my.cnf home/teeworlds/.gitconfig etc/zsh etc/vim* etc/mysql etc/ssmtp etc/dnsmasq.d etc/dnsmasq.conf etc/resolv.dnsmasq.conf etc/resolv.conf etc/resolv.conf.ddnet etc/systemd/system/dnsbl-iphub.service etc/security/limits.conf root/weekly root/.config root/.vim* root/.ssh* root/.z* usr/local/bin/ni usr/local/bin/rni etc/init.d/teeworlds-servers etc/network/if-up.d/iptables etc/apt/apt.conf.d/99defaultrelease etc/apt/sources.list.d etc/timezone etc/apt/apt.conf.d/50unattended-upgrades var/spool/cron/crontabs
 # scp ddnet-setup.tar.gz kor.ddnet.tw:/
 
 if [ "$#" -ne 2 ]; then
@@ -17,7 +17,7 @@ NAME_SQL=`echo $NAME_UPPER | head -c3`
 
 apt-get -y update
 apt-get -y upgrade
-apt-get -y install bsdutils tree zsh vim htop git g++ libboost-dev python-msgpack python-requests python3-requests python-dnspython python-mysqldb sshfs tcpdump gdb pkg-config ntpdate ntp mailutils msmtp msmtp-mta libssl-dev libmariadbclient-dev libmysqlcppconn-dev cmake make unattended-upgrades apt-listchanges iptables-persistent libwebsockets-dev libcurl4-openssl-dev python3 python3-dnslib python3-cachetools dnsmasq strace dnsutils sqlite3 libsqlite3-dev mariadb-client rsync libreadline-dev binutils-dev libpcap-dev libnl-genl-3-dev dh-autoreconf conntrack ncdu iperf3 psmisc ethtool net-tools mtr-tiny adduser cron iptables wget screen libmaxminddb-dev
+apt-get -y install bsdutils tree zsh vim htop git g++ libboost-dev python3-requests sshfs tcpdump gdb pkg-config ntpdate ntp mailutils msmtp msmtp-mta libssl-dev libmariadb-dev-compat libmariadb-dev libmysqlcppconn-dev cmake make unattended-upgrades apt-listchanges iptables-persistent libwebsockets-dev libcurl4-openssl-dev python3 python3-dnslib python3-cachetools dnsmasq strace dnsutils sqlite3 libsqlite3-dev mariadb-client rsync libreadline-dev binutils-dev libpcap-dev libnl-genl-3-dev dh-autoreconf conntrack ncdu iperf3 psmisc ethtool net-tools mtr-tiny adduser cron iptables wget screen libmaxminddb-dev unzip curl
 
 hostnamectl set-hostname ddnet$NAME_LOWER
 addgroup teeworlds
@@ -35,6 +35,8 @@ iptables -A serverinfo -s 37.187.108.123 -j ACCEPT
 iptables -A serverinfo -m hashlimit --hashlimit-above 1000/s --hashlimit-burst 2500 --hashlimit-mode dstport --hashlimit-name si_dstport -j DROP
 iptables -A serverinfo -m hashlimit --hashlimit-above 20/s --hashlimit-burst 100 --hashlimit-mode srcip --hashlimit-name si_srcip -j DROP
 iptables -I INPUT -s 185.82.223.0/24 -j DROP
+iptables -A INPUT -p tcp -m tcp --sport 35601 -j ACCEPT
+iptables -A INPUT -p tcp -m tcp -m conntrack -m multiport --ctstate NEW ! --dports 6546,22 -j DROP
 iptables-save > /etc/iptables.up.rules
 
 tar -C / -xvf ddnet-setup.tar.gz
@@ -51,11 +53,13 @@ dpkg-reconfigure -f noninteractive tzdata
 apt-get -y update
 apt-get -y -t testing install libmysqlcppconn-dev libmysqlcppconn7v5
 
+echo "nameserver 127.0.0.1" > /etc/resolv.conf.ddnet
+chattr +i /etc/resolv.conf.ddnet
 chsh --shell /usr/bin/zsh
 sed -i "s/hostname=.*/hostname=$NAME_LOWER.ddnet.tw/" /etc/ssmtp/ssmtp.conf
 systemctl enable teeworlds-servers
 
-echo "8298 8300 8303 8304 8305 8306 8308 8309 8311 8312" > /home/teeworlds/servers/all-servers
+echo "8298 8300 8303 8304 8305 8306 8308 8309" > /home/teeworlds/servers/all-servers
 sed -i "s/^sv_sql_servername .*/sv_sql_servername \"$NAME_SQL\"/" /home/teeworlds/servers/mysql.cfg
 sed -i "s/^USER = .*/USER = \"ddnet$NAME_LOWER\"/" /home/teeworlds/servers/serverstatus-client.py
 sed -i "s/^sv_name \"DDNet [A-Za-z0-9]* /sv_name \"DDNet $NAME_INGAME /" /home/teeworlds/servers/types/*/flexname.cfg /home/teeworlds/servers/servers/*.cfg
