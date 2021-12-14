629ab30b853004192cc2395a0c5879be70606805
def <dennis@felsin9.de>
2021-12-14 01:33:41 +0100

    Add arm64 macOS build instructions (untested)

--- ddnet-lib-update.sh
+++ ddnet-lib-update.sh
@@ -450,3 +450,80 @@ cd ../libpng-1.6.37
 ./configure --host=x86_64-apple-darwin17
 make -j4
 cp .libs/libpng16.16.dylib ..
+
+# Requires osxcross with SDK >= 12.0 for oa64-clang
+mkdir macarm
+cd macarm
+tar xvf ../curl-7.79.0.tar.gz
+tar xvf ../libogg-1.3.5.tar.gz
+tar xvf ../opus-1.3.1.tar.gz
+tar xvf ../opusfile-0.12.tar.gz
+tar xvf ../SDL2-2.0.18.tar.gz
+tar xvf ../11.0.tar.gz
+tar xvf ../sqlite-autoconf-3360000.tar.gz
+tar xvf ../x264-master.tar.bz2
+tar xvf ../ffmpeg-4.4.tar.gz
+tar xvf ../v4.2-stable.tar.gz
+tar xvf ../libpng-1.6.37.tar.gz
+
+export PATH=/home/deen/git/osxcross/target/bin/:$PATH
+export CC=oa64-clang
+export CXX=oa64-clang++
+eval `osxcross-conf`
+
+cd curl-7.79.0
+CFLAGS="-mmacosx-version-min=10.9" ./configure --host=arm64-apple-darwin17 --without-ssl --with-secure-transport --enable-static --enable-shared --disable-dict --disable-gopher --disable-imap --disable-pop3 --disable-rtsp --disable-smtp --disable-telnet --disable-tftp --disable-smb --disable-ldap --enable-file
+make -j4
+cp lib/.libs/libcurl.a ..
+
+cd ../libogg-1.3.5
+./configure CFLAGS="-mmacosx-version-min=10.9" --host=arm64-apple-darwin17
+make -j4
+cp src/.libs/libogg.a ..
+
+cd ../opus-1.3.1
+./configure CFLAGS="-mmacosx-version-min=10.9" --host=arm64-apple-darwin17
+make -j4
+cp .libs/libopus.a ..
+
+cd ../opusfile-0.12
+PKG_CONFIG=/usr/sbin/pkg-config DEPS_LIBS="-lopus -logg -L/home/deen/isos/ddnet/debian6/root/macarm/opus-1.3.1/.libs/ -L/home/deen/isos/ddnet/debian6/root/macarm/libogg-1.3.5/src/.libs/" ./configure CFLAGS="-mmacosx-version-min=10.9 -I/home/deen/isos/ddnet/debian6/root/macarm/opus-1.3.1/include -I/home/deen/isos/ddnet/debian6/root/macarm/libogg-1.3.5/include" CPPFLAGS="-I/home/deen/isos/ddnet/debian6/root/macarm/opus-1.3.1/include -I/home/deen/isos/ddnet/debian6/root/macarm/libogg-1.3.5/include" --host=arm64-apple-darwin17 --disable-http
+make -j4
+cp .libs/libopusfile.a ..
+
+cd ../SDL2-2.0.18
+patch -p1 < ../../4306.diff
+patch -p1 < ../../4683.diff
+./configure --enable-ime CFLAGS="-mmacosx-version-min=10.9" --host=arm64-apple-darwin17
+CFLAGS="-mmacosx-version-min=10.9" make -j4
+cp build/.libs/libSDL2-2.0.0.dylib ../SDL2
+
+cd ../11.0
+./configure CFLAGS="-mmacosx-version-min=10.9" --host=arm64-apple-darwin17 --with-png=no --with-bzip2=no --with-zlib=no --with-harfbuzz=no
+make -j4
+cp objs/.libs/libfreetype.6.dylib ..
+
+cd ../sqlite-autoconf-3360000
+./configure --host=arm64-apple-darwin17 CFLAGS="-fPIC -DSQLITE_OMIT_LOAD_EXTENSION"
+make -j4
+cp .libs/libsqlite3.0.dylib ..
+
+cd ../x264-master
+AS=nasm CFLAGS="-mmacosx-version-min=10.9 -I/usr/arm64-apple-darwin17/include" LDFLAGS="-L/usr/arm64-apple-darwin17/lib" ./configure --enable-static --disable-cli --disable-gpl --disable-avs --disable-swscale --disable-lavf --disable-ffms --disable-gpac --disable-lsmash --disable-interlaced --host=arm64-apple-darwin17 --prefix=/usr/arm64-apple-darwin17 --cross-prefix=arm64-apple-darwin17-
+make -j4
+
+cd ../ffmpeg-4.4
+./configure --disable-all --disable-appkit --disable-bzlib --disable-avfoundation --disable-coreimage --disable-securetransport --disable-audiotoolbox --disable-cuda-llvm --disable-videotoolbox --disable-alsa --disable-iconv --disable-libxcb --disable-libxcb-shape --disable-libxcb-xfixes --disable-sdl2 --disable-xlib --disable-zlib --enable-avcodec --enable-avformat --enable-encoder=libx264,aac --enable-muxer=mp4,mov --enable-protocol=file --enable-libx264 --enable-swresample --enable-swscale --enable-gpl --extra-cflags="-mmacosx-version-min=10.9 -I../x264-master" --extra-cxxflags="-mmacosx-version-min=10.9 -I../x264-master" --extra-ldflags="-L../x264-master" --arch=x86_64 --target_os=darwin --cross-prefix=arm64-apple-darwin17- --disable-static --enable-shared --cc=$CC --cxx=$CXX
+make -j4
+cp libavcodec/libavcodec.58.dylib libavformat/libavformat.58.dylib libavutil/libavutil.56.dylib libswresample/libswresample.3.dylib libswscale/libswscale.5.dylib ..
+
+cd ../libwebsockets-4.2-stable
+# own cross-osx.cmake
+cmake -DCMAKE_TOOLCHAIN_FILE=contrib/cross-osx.cmake -DLWS_WITH_SSL=OFF -DLWS_UNIX_SOCK=OFF -DLWS_WITHOUT_EXTENSIONS=ON -DLWS_WITH_SYS_SMD=OFF .
+make -j4
+cp lib/libwebsockets.18.dylib ..
+
+cd ../libpng-1.6.37
+./configure --host=arm64-apple-darwin17
+make -j4
+cp .libs/libpng16.16.dylib ..
