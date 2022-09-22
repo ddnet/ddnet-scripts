6b8bb45db06b040c596cb296f0d89eba72be8a6c
Dennis Felsing <dennis@felsin9.de>
2022-09-23 01:10:55 +0200

    ddnet.tw -> ddnet.org

--- ddnet-setup.sh
+++ ddnet-setup.sh
@@ -3,7 +3,7 @@
 # From root @ other server:
 # cd /
 # tar --exclude='UBSAN*' --exclude='ASAN*' --exclude='ddnet-server*.sqlite' --exclude='ddnet-block.sqlite' --exclude='teehistorian' --exclude='*.sql' --exclude='*.log*' --exclude='*.fifo' --exclude='nohup.out' -czf ddnet-setup.tar.gz home/teeworlds/servers home/teeworlds/dnsbl home/teeworlds/run-all.sh home/teeworlds/.config home/teeworlds/.vim* home/teeworlds/.ssh* home/teeworlds/.z* home/teeworlds/.my.cnf home/teeworlds/.gitconfig etc/zsh etc/vim* etc/mysql etc/ssmtp etc/dnsmasq.d etc/dnsmasq.conf etc/resolv.dnsmasq.conf etc/resolv.conf etc/resolv.conf.ddnet etc/systemd/system/dnsbl-iphub.service etc/security/limits.conf root/weekly root/ipset.sh root/.config root/.vim* root/.ssh* root/.z* usr/local/bin/ni usr/local/bin/rni etc/init.d/teeworlds-servers etc/network/if-up.d/iptables etc/apt/apt.conf.d/99defaultrelease etc/apt/sources.list.d etc/timezone etc/apt/apt.conf.d/50unattended-upgrades var/spool/cron/crontabs
-# scp ddnet-setup.tar.gz kor.ddnet.tw:/
+# scp ddnet-setup.tar.gz kor.ddnet.org:/
 
 if [ "$#" -ne 2 ]; then
   echo "Usage: $0 kor \"Korea\""
@@ -67,7 +67,7 @@ dpkg-reconfigure -f noninteractive tzdata
 echo "nameserver 127.0.0.1" > /etc/resolv.conf.ddnet
 chattr +i /etc/resolv.conf.ddnet
 chsh --shell /usr/bin/zsh
-sed -i "s/hostname=.*/hostname=$NAME_LOWER.ddnet.tw/" /etc/ssmtp/ssmtp.conf
+sed -i "s/hostname=.*/hostname=$NAME_LOWER.ddnet.org/" /etc/ssmtp/ssmtp.conf
 systemctl enable teeworlds-servers
 
 echo "8298 8300 8303 8304 8305 8306 8308 8309" > /home/teeworlds/servers/all-servers
@@ -105,7 +105,7 @@ su - teeworlds "./run-all.sh"
 # Add the IP on Cloudflare and other firewall we use for db server
 # Make sure teehistorian backups work and inform other teehistorian consumers
 #
-# On ddnet.tw:
+# On ddnet.org:
 # vim ServerStatus/server/config.json
 # systemctl restart sergate
 # cd servers
