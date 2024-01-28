90db2ccc2b7f7718902c058c41557c2b94e06964
Dennis Felsing <dennis@felsing.org>
2024-01-28 15:16:14 +0100

    No more variables.h

--- ddnet-setup.sh
+++ ddnet-setup.sh
@@ -19,6 +19,7 @@ apt-get -y update
 apt-get -y upgrade
 apt-get -y dist-upgrade
 apt-get -y install bsdutils tree zsh vim htop git g++ libboost-dev python3-requests sshfs tcpdump gdb pkg-config ntpdate ntp mailutils msmtp msmtp-mta libssl-dev libmariadb-dev-compat libmariadb-dev libmysqlcppconn-dev cmake make unattended-upgrades apt-listchanges iptables-persistent libwebsockets-dev libcurl4-openssl-dev python3 python3-dnslib python3-cachetools dnsmasq strace dnsutils sqlite3 libsqlite3-dev mariadb-client rsync libreadline-dev binutils-dev libpcap-dev libnl-genl-3-dev dh-autoreconf conntrack ncdu psmisc ethtool net-tools mtr-tiny adduser cron iptables wget screen libmaxminddb-dev unzip curl curl screen ipset curl zstd python3-pip
+rm -rf  /root/.acme.sh
 rm -rf ~/.pip # bad tencent server on some CHN locations
 if [[ "$NAME_SQL" == "CHN" ]]; then
   pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
